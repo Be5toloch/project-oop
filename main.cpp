5e96e883f29bfe99b0f90cf39bdd0e734fe70d1c
Be5toloch <pasha.koyakov@gmail.com>
2024-06-24 15:13:04 +0200

    third commit

--- main.cpp
+++ main.cpp
@@ -28,3 +28,25 @@ public:
         cout << "Choosing water extinguisher and fire blanket for small fire." << endl;
     }
 };
+
+class MediumFireFighter : public FireFighter {
+public:
+    void fightFire() override {
+        cout << "Medium fire at " << address << ". Using CO2 extinguisher." << endl;
+    }
+
+    void chooseEquipment() override {
+        cout << "Choosing CO2 extinguisher and protective gear for medium fire." << endl;
+    }
+};
+
+class LargeFireFighter : public FireFighter {
+public:
+    void fightFire() override {
+        cout << "Large fire at " << address << ". Using foam extinguisher and fire hose." << endl;
+    }
+
+    void chooseEquipment() override {
+        cout << "Choosing foam extinguisher and fire hose for large fire." << endl;
+    }
+};
